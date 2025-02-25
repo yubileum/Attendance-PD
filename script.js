41a538084262c6719b9711c6add651ec0ebaf5d2
Michael Yubileum <45558581+yubileum@users.noreply.github.com>
2025-02-25 23:44:59 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -8,13 +8,13 @@ function checkIn() {
 
     document.getElementById("loading").style.display = "flex"; // Tampilkan loading screen
 
-    fetch("https://script.google.com/macros/s/AKfycbwiU2KKWNp8kUnEN_Z3OiLSEEF1jOsZPdl-gIyzWMFrSwgjj0gJQHGqmJ7HFfohTtcUEg/exec?phone=" + encodeURIComponent(phoneNumber), { cache: "no-store" })
+    fetch("https://script.google.com/macros/s/AKfycbw9fLuV4jeAR9EjFZgWUtibw00RfDSe-Den5eGBw7wacnDld7CNfKqwJc2rfnqIdfTBGw/exec?phone=" + encodeURIComponent(phoneNumber), { cache: "no-store" })
         .then(response => response.json())
         .then(data => {
             document.getElementById("loading").style.display = "none"; // Sembunyikan loading screen
 
             if (data.success) {
-                document.getElementById("message").innerHTML = `Welcome <span>${data.name}</span>`;
+                document.getElementById("message").innerHTML = `Shalom <span>${data.name}</span>`;
             } else {
                 document.getElementById("message").innerText = "Phone number not found.";
             }            
