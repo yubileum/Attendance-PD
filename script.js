6906396ccab3e792e9ef27b8f145b5c07a0031ba
Michael Yubileum <45558581+yubileum@users.noreply.github.com>
2025-03-01 01:21:13 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -8,15 +8,15 @@ function checkIn() {
 
     document.getElementById("loading").style.display = "flex"; // Tampilkan loading screen
 
-    fetch("https://script.google.com/macros/s/AKfycbxC0_vMwfrD62VUgtbfD7XZNxpVZMQlJUW-GmalA5ITKXjrmHaQybUFQL_6edi8_D3P/exec?phone=" + encodeURIComponent(phoneNumber), { cache: "no-store" })
+    fetch("https://script.google.com/macros/s/AKfycbxbXW5WQUBvyo4YhyPuvpQLuLC30nVqRwUPbGTrdMNuW1t35wDFakJ04nhApvWCem8/exec?phone=" + encodeURIComponent(phoneNumber), { cache: "no-store" })
         .then(response => response.json())
         .then(data => {
             document.getElementById("loading").style.display = "none"; // Sembunyikan loading screen
 
             if (data.success) {
-                document.getElementById("message").innerHTML = `Shalom <span>${data.name}</span>`;
+                document.getElementById("message").innerHTML = `Shalom <span>${data.name}</span> </br> We're happy to see you!`;
             } else {
-                document.getElementById("message").innerText = "Phone number not found.";
+                document.getElementById("message").innerHTML = `Nomor HP belum terdaftar, registrasi dulu di <a href="https://forms.gle/WJWFGg69V8WtSjqf7" target="_blank">https://forms.gle/WJWFGg69V8WtSjqf7</a>`;
             }            
             document.getElementById("popup").style.display = "flex";
         })
